7302383b5001e1b11e18011084d82a7fdde9c222
Aditya Sharma <adityasharma1709@gmail.com>
2023-12-09 01:44:57 -0800

    fixed random locs ratio

--- backend/eval/stage_3/find_coords.py
+++ backend/eval/stage_3/find_coords.py
@@ -4,25 +4,24 @@ import os
 import cv2
 import numpy as np
 
-imgs = os.listdir("../all_random_locs")
-imgs = sorted(imgs)
-print(imgs)
-
+RATIO = 1.15694130137
 output = {}
-for image in imgs:
-    src = cv2.imread(image)
-    hsv = cv2.cvtColor(src, cv2.COLOR_BGR2HSV)
-    mask = cv2.inRange(hsv, np.array([0, 200, 200]), np.array([0, 255, 255]))
 
-    index = np.where(mask == 255)
-    x, y = np.mean(index[0]), np.mean(index[1])
+for image in sorted(os.listdir("../../all_random_locs")):
+    path = "../../all_random_locs/" + image
     img_name = image.split("_")[0] + ".png"
     obj_name = image.split("_")[1].split(".")[0]
 
+    src = cv2.imread(path)
+    hsv = cv2.cvtColor(src, cv2.COLOR_BGR2HSV)
+    mask = cv2.inRange(hsv, np.array([0, 200, 200]), np.array([0, 255, 255]))
+    index = np.where(mask == 255)
+    x, y = np.mean(index[0]), np.mean(index[1])
+
     if output.get(img_name) is None:
         output[img_name] = {}
-        output[img_name][obj_name] = [x, y]
+        output[img_name][obj_name] = [y * RATIO, x * RATIO]
     else:
-        output[img_name][obj_name] = [x, y]
+        output[img_name][obj_name] = [y * RATIO, x * RATIO]
 
 json.dump(output, open("coords.json", "w"))
