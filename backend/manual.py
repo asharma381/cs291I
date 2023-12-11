b0cabdffae3b2c8184a432acf2cccc6ea501e665
Luke Yoffe <lukeyoffe@umail.ucsb.edu>
2023-12-11 02:37:19 -0800

    update manual eval

--- backend/manual.py
+++ backend/manual.py
@@ -1,93 +1,69 @@
 # %%
 import os
 import matplotlib.pyplot as plt
-from random import shuffle, randint
+from random import randint
 from collections import Counter, defaultdict
+from IPython.display import display, Markdown
 
 # %%
-thing = "good"
-img_file_list = os.listdir(f"all_{thing}_locs")
-
-# other_file_list = os.listdir(f"all_octopus_vqa_locs")
-
-# img_file_list = list(set(img_file_list) & set(other_file_list))
 
 
-shuffle(img_file_list)
+METHOD_1 = "all_bad_locs"
+METHOD_2 = "all_good_locs"
+
+image_path_prefix = "images/annotated"
+img_file_list = sorted(
+    set(os.listdir(f"{image_path_prefix}/{METHOD_1}"))
+    & set(os.listdir(f"{image_path_prefix}/{METHOD_2}"))
+)
 
 counts = Counter()
 object_counts = defaultdict(lambda: Counter())
-# map1 = {
-#     "[": 0,
-#     "]": 1,
-# }
-
-# map2 = {
-#     0: "good",
-#     1: "ours",
-# }
 
 # %%
 
-ls = img_file_list[:50]
-len_ls = len(ls)
+ls = img_file_list[:3]
 for i, file in enumerate(ls):
-    print(file)
-
-    print(f"all_{thing}_locs/{file}")
     object = file.split("_")[1].split(".")[0]
-    print(object)
-    fig, (ax1, ax2) = plt.subplots(1, 2, figsize=(10, 15), gridspec_kw = {'wspace': 0, 'hspace': 0})
-    img_old = plt.imread(f"all_{thing}_locs/{file}")
-    img_vqa = plt.imread(f"all_random_locs/{file}")
+    display(Markdown(f"In the image below, where should a **{object}** be placed?"))
+    fig, (ax1, ax2) = plt.subplots(
+        1, 2, figsize=(10, 15), gridspec_kw={"wspace": 0, "hspace": 0}
+    )
+    img_1 = plt.imread(f"{image_path_prefix}/{METHOD_1}/{file}")
+    img_2 = plt.imread(f"{image_path_prefix}/{METHOD_2}/{file}")
 
-    rand = randint(0,1)
+    rand = randint(0, 1)
     if rand == 0:
-        ax1.imshow(img_old)
+        ax1.imshow(img_1)
         ax1.axis("off")
-        ax2.imshow(img_vqa)
+        ax2.imshow(img_2)
         ax2.axis("off")
         # fig.show()
         plt.show()
     else:
-        ax1.imshow(img_vqa)
+        ax1.imshow(img_2)
         ax1.axis("off")
-        ax2.imshow(img_old)
+        ax2.imshow(img_1)
         ax2.axis("off")
         # fig.show()
         plt.show()
-    
-
 
     ret = input("Enter '[' or ']' or anything else")
     if ret in {"[", "]"}:
         if (rand == 0 and ret == "[") or (rand == 1 and ret == "]"):
-            counts[thing] += 1
-            object_counts[object][thing] += 1
+            counts[METHOD_1] += 1
+            object_counts[object][METHOD_1] += 1
         else:
-            counts["ours"] += 1
-            object_counts[object]["ours"] += 1
+            counts[METHOD_2] += 1
+            object_counts[object][METHOD_2] += 1
     else:
         counts["tie"] += 1
         object_counts[object]["tie"] += 1
 
+    print(f"{i + 1}/{len(ls)}")
 
-
-    # if ret in map1:
-    #     best = map1[ret]
-        
-    #     counts[map2[(best + rand) % 2]] += 1
-    
-    print(f"{i + 1}/{len_ls}")
-    
-    print(counts)
-    print(dict(object_counts))
-
-
-
-    
-        
-
+print(dict(counts))
+print(dict(object_counts))
 
 
 # %%
